867a8b377edb8f57670c0be840616ec3b3f06cd8
Heverson <heversonbenatti@hotmail.com>
2023-05-10 09:39:22 -0300

    Implementação de turnos, fichas e decisões

--- poker.py
+++ poker.py
@@ -30,7 +30,7 @@ def distribuir_cartas(baralho):
     baralho = [carta for carta in baralho if carta not in mao]
     return mao, baralho
 
-def distribuir_cartas_comunitarias(baralho):
+def gerar_flop(baralho):
     baralho = list(baralho)  # Converter o baralho para lista
     mesa = random.sample(baralho, 5)
     baralho = [carta for carta in baralho if carta not in mesa]
@@ -41,7 +41,8 @@ def gerar_jogador(baralho, nome):
     jogador['nome'] = nome
     jogador['mao'], baralho = distribuir_cartas(baralho)
     jogador['forca_mao'] = 0
-    jogador['decisao'] = 'apostar'
+    jogador['decisao'] = ' '
+    jogador['fichas'] = 5000
     return jogador, baralho
 
 def determinar_forca_mao_jogador(jogador, mesa, evaluator):
@@ -55,31 +56,73 @@ def determinar_vencedor(jogador1, jogador2):
         return jogador1
     elif jogador2['forca_mao'] < jogador1['forca_mao']:
         return jogador2
+    
+def pergunta1(jogador1, jogador2, pote):
+    decisao = input(jogador1['nome'] + ":\n 1 - Apostar\n 2 - Desistir?\n> ")
+    if decisao == '1':
+        jogador1['decisao'] = 'apostar'
+        aposta1 = int(input("Valor: "))
+        jogador1['fichas'] -= aposta1
+        pote += aposta1
+        jogador1, jogador2, pote = pergunta2(jogador1, jogador2, pote, aposta1)
+    else:
+        jogador1['decisao'] = 'desistir'
+        jogador2['fichas'] += pote
+        pote = 0
+    return jogador1, jogador2, pote
+
+def pergunta2(jogador1, jogador2, pote, aposta):
+    decisao = input(jogador2['nome'] + ":\n 1 - Pagar\n 2 - Apostar\n 3 - Desistir?\n> ")
+    if decisao == '1':
+        jogador2['decisao'] = 'pagar'
+        jogador2['fichas'] -= aposta
+        pote += aposta
+    elif decisao == '2':
+        jogador2['decisao'] = 'apostar'
+        aposta1 = int(input("Valor: "))
+        jogador2['fichas'] -= aposta1
+        pote += aposta1
+        jogador1, jogador2, pote = pergunta2(jogador2, jogador1, pote, aposta1-aposta)
+    else:
+        jogador2['decisao'] = 'desistir'
+        jogador1['fichas'] += pote
+        pote = 0
+    return jogador1, jogador2, pote
 
 def jogar_poker(baralho, evaluator):
     jogador1, baralho = gerar_jogador(baralho, 'Jogador 1')
     jogador2, baralho = gerar_jogador(baralho, 'Jogador 2')
+    pote = 0
 
-    mesa, baralho = distribuir_cartas_comunitarias(baralho)
+    print("=== JOGADOR 1 ===")
+    print("Cartas do Jogador 1:", jogador1['mao'])
+    print("=== JOGADOR 2 ===")
+    print("Cartas do Jogador 2:", jogador2['mao'])
 
-    # print("=== JOGADOR 1 ===")
-    # print("Cartas do Jogador 1:", jogador1['mao'])
-    # print("=== JOGADOR 2 ===")
-    # print("Cartas do Jogador 2:", jogador2['mao'])
-    # print("=== MESA ===")
-    # print("Cartas da Mesa:", mesa)
-    # print("=== BARALHO ===")
-    # print("Cartas Restantes no Baralho:", baralho)
-
-    if jogador1['decisao'] == 'apostar' and jogador2['decisao'] == 'apostar':
-        jogador1['forca_mao'] = determinar_forca_mao_jogador(jogador1, mesa, evaluator)
-        jogador2['forca_mao'] = determinar_forca_mao_jogador(jogador2, mesa, evaluator)
-        vencedor = determinar_vencedor(jogador1, jogador2)
-
-        salvar_simulacao(jogador1, jogador2, mesa, vencedor)
+    jogador1, jogador2, pote = pergunta1(jogador1, jogador2, pote)
+    mesa, baralho = gerar_flop(baralho)
 
 
-        return vencedor
+
+
+    # for turno in range(5):
+    #     if turno == 0:
+
+    #         if jogador1['decisao'] == 'apostar' and jogador2['decisao'] == 'apostar':
+    #             mesa, baralho = gerar_flop(baralho)
+
+    print("=== MESA ===")
+    print("Cartas da Mesa:", mesa)
+
+    print("=== BARALHO ===")
+    print("Cartas Restantes no Baralho:", baralho)
+    jogador1['forca_mao'] = determinar_forca_mao_jogador(jogador1, mesa, evaluator)
+    jogador2['forca_mao'] = determinar_forca_mao_jogador(jogador2, mesa, evaluator)
+    vencedor = determinar_vencedor(jogador1, jogador2)
+
+    # salvar_simulacao(jogador1, jogador2, mesa, vencedor)
+
+    return jogador1, jogador2, pote, vencedor
 
 def simular_jogos(num_simulacoes):
     baralho = set(gerar_baralho())
@@ -88,12 +131,22 @@ def simular_jogos(num_simulacoes):
     resultados = []
 
     for _ in tqdm(range(num_simulacoes), desc='Simulações'):
-        vencedor = jogar_poker(baralho, evaluator)
+        jogador1, jogador2, pote, vencedor = jogar_poker(baralho, evaluator)
         if vencedor:
-            # print("=== RESULTADO ===")
-            # print("Vencedor:", vencedor['nome'])
+            vencedor['fichas'] += pote
+            print(jogador1['nome'])
+            print(jogador1['fichas'])
+            print(jogador1['decisao'])
+            print("\n")
+            print(jogador2['nome'])
+            print(jogador2['fichas'])
+            print(jogador2['decisao'])
+            print(pote)
+            print("=== RESULTADO ===")
+            print("Vencedor:", vencedor['nome'])
             resultados.append(vencedor)
 
     return resultados
 
-resultados = simular_jogos(100000000)
+resultados = simular_jogos(1)
+
